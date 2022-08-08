82369e0af1bad4fe6a5e90909060cbf594124a6f
HeroSima43 <pwd@list.ru>
2022-08-08 22:08:56 +0300

    add task7

---
+++ Seminar1/task7/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите трехзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int A = N / 100;
+int B = (N - (A * 100)) / 10;
+int C = N - (A * 100  + B * 10);
+Console.Write(C);
