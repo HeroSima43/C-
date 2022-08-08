49bf78d58335d64aadace08031c25482fc992d28
HeroSima43 <pwd@list.ru>
2022-08-08 22:03:36 +0300

    add task1

---
+++ Seminar1/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число #1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число #2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("Да!");
+}
+else
+{
+    Console.WriteLine("Нет!");
+} 
