0258a975e6296e3dff96ce24798af069ca5c154b
HeroSima43 <pwd@list.ru>
2022-08-08 22:06:56 +0300

    add task5

---
+++ Seminar1/task5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = -N;
+while (count <= N)
+{
+    Console.Write(" ");
+    Console.Write(count);
+    count = count + 1;
+}
