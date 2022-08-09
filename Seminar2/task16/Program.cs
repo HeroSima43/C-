c5a9d6a2d2d2fea339623a88404a06c02a63eba9
HeroSima43 <pwd@list.ru>
2022-08-09 15:15:24 +0300

    add task16

---
+++ Seminar2/task16/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число #1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число #2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int sqrA = numberA * numberA;
+int sqrB = numberB * numberB;
+if (numberA == sqrB ^ numberB == sqrA)
+{
+    Console.WriteLine("ДА!");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
