49c5cbd963bd79fedbac0717bd611ab6044b85f4
HeroSima43 <pwd@list.ru>
2022-08-09 14:58:15 +0300

    add task12

---
+++ Seminar2/task12/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число #1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число #2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int ostatok = numberA % numberB;
+if (ostatok == 0)
+{
+    Console.WriteLine("Кратно!");
+}
+else
+{
+    Console.WriteLine($"Остаток от деления: {ostatok}");
+}
