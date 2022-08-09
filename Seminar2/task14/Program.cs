7ae78d2cf1747028d7f9c3f64b9b6cdc859c4931
HeroSima43 <pwd@list.ru>
2022-08-09 15:08:30 +0300

    add task14

---
+++ Seminar2/task14/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0) 
+{
+    Console.WriteLine("Кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine("Не кратно!");
+}
+
