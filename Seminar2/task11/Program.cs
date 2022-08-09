2abf237943bd7e530a7aba97bb7f5d5d9b8e941f
HeroSima43 <pwd@list.ru>
2022-08-09 14:50:08 +0300

    add task11

---
+++ Seminar2/task11/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Наше случайное число: {number}");
+int digit1 = number / 100;
+int digit2 = number / 10;
+int digit3 = number % 10;
+
+int numberX = digit1 * 10 + digit3;
+Console.WriteLine(numberX);
