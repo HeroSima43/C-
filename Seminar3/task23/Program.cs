bcdd4c67fd9359c5586e1db60520f07776d5dde4
HeroSima43 <pwd@list.ru>
2022-08-12 20:33:07 +0300

    add task23

---
+++ Seminar3/task23/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+N = Math.Abs(N);
+int count = 1;
+int sqr = 0;
+while (count <= N)
+{
+    sqr = count * count * count;
+    Console.WriteLine($"Куб числа {count} равен: {sqr}");
+    count++;
+}
