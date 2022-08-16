4c6d84f043f00369a34722be900c67ec1642aa6d
HeroSima43 <pwd@list.ru>
2022-08-16 11:01:44 +0300

    add task28

---
+++ Seminar4/task28/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int Cycle(int a = 1)
+{   
+    int pro = 1;
+    for (int i = 1; i <= a; i++)
+    {
+        pro = pro * i;
+    }
+    return pro;
+}
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {Cycle(N)}");
