65331be0d43c1a0e79030094868e10f25e088cf5
HeroSima43 <pwd@list.ru>
2022-08-17 00:11:58 +0300

    add task27

---
+++ Seminar4/task27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+string B = Convert.ToString(A);
+int N = B.Length;
+int sum = 0;
+for (int i = 0; i < N; i++)
+{
+    sum = sum + int.Parse(B[i].ToString());
+}
+Console.WriteLine($"Сумма цифр в числе равна: {sum}");
