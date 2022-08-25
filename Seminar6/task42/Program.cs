17d3397acfe3a13d1160cd1105b5b69d0a2786b5
HeroSima43 <pwd@list.ru>
2022-08-25 13:34:02 +0300

    add task42

---
+++ Seminar6/task42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+void PrintBinary(int a)
+{
+    if (a <= 0)
+    {
+        return;
+    }
+    PrintBinary(a/2);
+    Console.Write(a % 2);
+}
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+PrintBinary(number);
