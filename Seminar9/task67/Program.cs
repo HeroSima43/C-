5479757b6d6c4b55c8dad0518692226a9a81a34f
HeroSima43 <pwd@list.ru>
2022-09-04 22:38:31 +0300

    add task67

---
+++ Seminar9/task67/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Задача 67:Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// // 453 -> 12
+// // 45 -> 9
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int SumA(int A)
+{
+    if (A == 0)
+    {
+        return 0;
+    }
+    return A % 10 + SumA(A / 10);
+}
+
+
+Console.WriteLine(SumA(A));
+
